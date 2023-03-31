8a410a303338a1ea327b1aa9f6515fa1712bb7fd
cosyrusty <priyaanyadav@gmail.com>
2023-03-31 11:02:12 +0530

    refactor and use stdout.lock()

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ echo in rust.
   $ cargo run -- -n hello
   $ cargo run -- -e 'hello\nworld'
   ```
+
+## sudo code
+  * read cmdline line arguments
+  * parse the options if any in args
+  * otherwise store the args in vec
+  * print each string in args according to parsed option
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
 use std::env;
+use std::error::Error;
+use std::io::{self, Write};
 
 const HELP: &str = "echo [OPTIONS...] [STRING...]";
 const VERSION: &str = "v0.1";
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut no_newline = false;
     let mut escaped = false;
-    let mut help = false;
-    let mut version = false;
 
     let mut values: Vec<String> = vec![];
     let mut parsed_option = false;
 
-    'a: for i in env::args().skip(1) {
+    for i in env::args().skip(1) {
         if !parsed_option && i.starts_with('-') {
             match &i[..] {
                 "-n" => no_newline = true,
                 "-e" => escaped = true,
                 "-E" => escaped = false,
                 "--help" => {
-                    help = true;
-                    break 'a;
+                    writeln!(io::stdout().lock(), "{}", HELP)?;
+                    return Ok(());
                 }
                 "--version" => {
-                    version = true;
-                    break 'a;
+                    writeln!(io::stdout().lock(), "{}", VERSION)?;
+                    return Ok(());
                 }
                 _ => {
                     values.push(i);
@@ -37,27 +37,30 @@ fn main() {
         }
     }
 
-    if help {
-        print!("{}", HELP)
-    } else if version {
-        print!("{}", VERSION)
-    } else if escaped {
-        print_escaped(values)
-    } else {
-        for (i, v) in values.iter().enumerate() {
-            if i > 0 {
-                print!(" {}", v)
-            } else {
-                print!("{}", v)
+    let mut output = io::stdout().lock();
+
+    for (i, input) in values.iter().enumerate() {
+        if i > 0 {
+            write!(&mut output, " ")?
+        }
+
+        if escaped {
+            let should_stop = print_escaped(input, &mut output)?;
+            if should_stop {
+                break;
             }
+        } else {
+            write!(output, "{input}")?;
         }
     }
 
     if !no_newline {
-        println!()
+        writeln!(output)?
     }
+
+    Ok(())
 }
 
-fn print_escaped(values: Vec<String>) {
-    print!("Ecs is not implimented yet!")
+fn print_escaped(v: &str, writer: impl Write) -> io::Result<bool> {
+    Ok(true)
 }
